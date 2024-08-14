3e619969f0ad7b931ebb82f4cc08d46819b6ab71
Xerox Deity <72761788+lizzahd@users.noreply.github.com>
2024-08-14 16:57:17 -0600

    added main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn conf() -> Conf {
+    Conf {
+        window_title: String::from("Unsteel Chez"),
+        window_width: 1280,
+        window_height: 800,
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(conf)]
+async fn main() {
+    let mut entities: Vec<Box<dyn Entity>> = Vec::new();
+
+    loop {
+        clear_background(BLACK);
+
+        for entity in &mut entities {
+            entity.update();
+        }
+
+        for entity in &mut entities {
+            entity.draw();
+        }
+
+        next_frame().await;
+    }
 }
