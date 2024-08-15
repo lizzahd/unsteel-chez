a8385ff73825ca9df43369d37a895eae8c86d9fc
Ivan Armstrong <armstrongivan7@gmail.com>
2024-08-14 17:29:41 -0700

    framework aaaaaaaaaa

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use macroquad::prelude::*;
 
 use crate::entittie::*;
+use crate::playa::*;
+use crate::utils::*;
 
 mod entittie;
+mod playa;
+mod utils;
 
 fn conf() -> Conf {
     Conf {
         window_title: String::from("Unsteel Chez"),
-        window_width: 1280,
-        window_height: 800,
+        window_width: 1152,
+        window_height: 648,
         fullscreen: false,
         ..Default::default()
     }
@@ -17,6 +21,10 @@ fn conf() -> Conf {
 #[macroquad::main(conf)]
 async fn main() {
     let mut entities: Vec<Box<dyn Entity>> = Vec::new();
+    entities.push(Box::new(Player::new(vec2(64., 64.))));
+
+    let mut hitboxes: Vec<Rect> = Vec::new();
+    hitboxes.push(Rect::new(0., 0., 32., 64.));
 
     loop {
         clear_background(BLACK);
@@ -27,6 +35,10 @@ async fn main() {
 
         for entity in &mut entities {
             entity.draw();
+        }
+
+        for hitbox in &mut hitboxes {
+            draw_rectangle_lines(hitbox.x, hitbox.y, hitbox.w, hitbox.h, 2., Color::from_rgba(0, 255, 0, 255));
         }
 
         next_frame().await;
--- src/playa.rs
+++ src/playa.rs
@@ -1,1 +1,31 @@
 // burt ass
+use macroquad::prelude::*;
+
+use crate::entittie::*;
+use crate::utils::*;
+
+poop::oop! (
+	public class Player {
+		pos: Vec2,
+		vel: Vec2,
+		hp: i32,
+	}
+
+	pub fn new(pos: Vec2) -> Self {
+		Self {
+			pos,
+			vel: vec2(0., 0.),
+			hp: 5,
+		}
+	}
+);
+
+impl Entity for Player {
+	fn draw(&self) {
+		
+	}
+
+	fn update(&mut self) {
+
+	}
+}
